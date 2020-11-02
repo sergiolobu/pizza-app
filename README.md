427ca3cc71106d40bb6d68d646ca953385dbd554
sergiolobu <slozanobueno@gmail.com>
2020-11-02 11:10:27 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+pizza-app
+=========
+
+A Symfony project created on November 2, 2020, 10:48 am.
---
+++ src/PizzaBundle/PizzaBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace PizzaBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PizzaBundle extends Bundle
+{
+}
